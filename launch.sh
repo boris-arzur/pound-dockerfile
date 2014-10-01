7afa3c24b6cc6040a1001af3bf153096568958a6
boris <boris@brzr.fr>
2014-10-01 10:01:21 +0200

    fronting for three services now.

--- launch.sh
+++ launch.sh
@@ -1,5 +1,10 @@
 set +e
-sed -i -e s/HTTP_PORT_80_TCP_ADDR/${HTTP_PORT_80_TCP_ADDR}/ -e s/HTTP_PORT_80_TCP_PORT/${HTTP_PORT_80_TCP_PORT}/ /pound.cfg.template
+apt-get update && apt-get upgrade -qy
+
+sed -i -e s/STATIC_PORT_80_TCP_ADDR/${STATIC_PORT_80_TCP_ADDR}/ -e s/STATIC_PORT_80_TCP_PORT/${STATIC_PORT_80_TCP_PORT}/ /pound.cfg.template
+sed -i -e s/SQ_PORT_80_TCP_ADDR/${SQ_PORT_80_TCP_ADDR}/ -e s/SQ_PORT_80_TCP_PORT/${SQ_PORT_80_TCP_PORT}/ /pound.cfg.template
+sed -i -e s/RC_PORT_80_TCP_ADDR/${RC_PORT_80_TCP_ADDR}/ -e s/RC_PORT_80_TCP_PORT/${RC_PORT_80_TCP_PORT}/ /pound.cfg.template
+
 mv /pound.cfg.template /etc/pound/pound.cfg
 cat /etc/pound/pound.cfg
 
